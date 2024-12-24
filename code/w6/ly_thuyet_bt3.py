83ea69a66a34cf91550b8f42b8fa5bc73dfc18e6
thuandd <thisthuan@gmail.com>
2024-12-24 09:49:06 +0700

    update bai 3

--- code/w6/ly_thuyet_bt3.py
+++ code/w6/ly_thuyet_bt3.py
@@ -2,3 +2,12 @@
 # Yêu cầu:
 # - đọc dữ liệu từ tệp tin: data_input.txt
 # - tính tổng tất cả các số trong tập tin.
+
+with open("data_input.txt", "r") as dl:
+    sum_data = 0
+    for li in dl:
+        li = li.strip()
+        li_split = li.split()
+        li_int = [int(i) for i in li_split]
+        sum_data = sum_data + sum(li_int)
+    print("tong la: ", sum_data)
